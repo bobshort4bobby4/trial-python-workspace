99f364b6b545b7ffe5ccc0eb8bab5d3a1db02e32
bobshort4bobby4 <treborken@gmail.com>
2022-01-04 16:36:10 +0000

    ADD data folder

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+"""
+trial to see if i could parse a json file.
+"""
+import json
+
+horsedata = []
+for line in open('twoline-horsepro.json', 'r'):
+    horsedata.append(json.loads(line))
+
+print(type(horsedata))
+print()
+print("lenght of the horsedate file is", len(horsedata))
+print("type of the fisrt line is", type(horsedata[0]))
+
+for key, value in horsedata[0].items():
+    print(f"Key: {key} Value {value}")
+    print("------------------")
