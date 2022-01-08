211c3c6dac89706ec2ecb70ff7469a159f8f5099
bobshort4bobby4 <treborken@gmail.com>
2022-01-08 17:16:00 +0000

    ADD plotting

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
-from matplotlib import pyplot as plo
+"""from matplotlib import pyplot as plo
 import matplotlib
 matplotlib.use('Agg')
 """
 x  = [1, 2, 3, 4, 5]
 y =   [2, 4, 6, 8, 10]
-
+"""
 plo.plot(x,y)
 plo.show()
 """
-print("Hello tristan")
+
+import plotext as plt
+
+plt.plot(x,y)
+plt.title("Line Plot")
+plt.show()
