96ece3ec3137e558399d0d32ef76ba511f01c889
bobshort4bobby4 <treborken@gmail.com>
2022-01-16 15:48:43 +0000

    ADD matplotlib

--- run.py
+++ run.py
@@ -1,16 +1,21 @@
-"""from matplotlib import pyplot as plo
-import matplotlib
-matplotlib.use('Agg')
-"""
+#from matplotlib import pyplot as plo
+#import matplotlib
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+mpl.use('Agg')
+print("trial for matplotlib")
 x  = [1, 2, 3, 4, 5]
 y =   [2, 4, 6, 8, 10]
-"""
-plo.plot(x,y)
-plo.show()
-"""
+print(x)
+print(mpl.__version__)
 
-import plotext as plt
-
-plt.plot(x,y)
-plt.title("Line Plot")
+plt.plot(x, y)
 plt.show()
+
+
+# import plotext as plt
+
+# plt.plot(x,y)
+# plt.title("Line Plot")
+# plt.show()
