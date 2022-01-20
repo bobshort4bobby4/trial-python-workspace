6ee24b4bd41f4e75b6ad8b421b47f16b8e9d6122
bobshort4bobby4 <treborken@gmail.com>
2022-01-20 22:08:13 +0000

    add draw function to code imported from idle

--- run.py
+++ run.py
@@ -1,16 +1,77 @@
-"""from matplotlib import pyplot as plo
-import matplotlib
-matplotlib.use('Agg')
-"""
-x  = [1, 2, 3, 4, 5]
-y =   [2, 4, 6, 8, 10]
-"""
-plo.plot(x,y)
-plo.show()
-"""
+board = [[],[],[],[],[],[],[]]
+number_turns = 2
+keep_going_eileen = True
+underl = "_"
 
-import plotext as plt
+for i in range(6): # fill board variable
+    for j in range(7):
+        board[i].insert(j,"_")
 
-plt.plot(x,y)
-plt.title("Line Plot")
-plt.show()
+print("Player 1 is x, Player 2  is 0")
+print(board.count("_"))
+def draw_board(board):        
+        print("")
+        print(" 0    2   4   6 ")
+        print("_________________")
+        for i in range(6):
+                print(i, "", end="")
+                for j in range(7):
+                        print(board[i][j], end="|")
+                print("")
+
+def check_column_full(col):
+    temp= board[0][col]
+    print(temp)
+    while board[0][col] != "_":
+            col = int(input("This column is full. Please choose again: "))
+    return col
+
+def take_input_from_player():
+    global number_turns
+    if number_turns % 2 == 0:
+        player_piece = "x"
+        player = "Player 1 "
+    else:
+        player = "Player 2"
+        player_piece = "0"
+    col = int(input(f"{player} pick a column to drop an {player_piece} :"))
+    choice = check_column_full(col)
+    for row in range(5, -1, -1):
+        if board[row][choice] == "_":
+                board[row][choice] = player_piece
+                number_turns +=1
+                break
+        #else:
+        #    print("That column is full, Pick another")
+          #  take_input_from_player()
+    
+def check_for_quit():
+    flag = input("enter y to quit").lower()
+    if flag == "y":
+        keep_going_eileen = False
+
+draw_board(board)
+
+def check_draw(board):
+    strboard = str(board)
+    temp = strboard.count("_")
+    print(temp)
+    if temp > 0:
+        return
+    else:
+        declare_draw()
+
+def declare_draw():
+    print("Neither of you were good enough to win") 
+    exit()
+             
+while keep_going_eileen:
+    take_input_from_player()
+    draw_board(board)
+    print(board.count("_"))
+    check_draw(board)
+    # check_for_quit()
+
+
+
+
