fa76fb3f227458a948d65ee461618f5077c850fe
bobshort4bobby4 <treborken@gmail.com>
2022-01-22 17:43:17 +0000

    ADD win check solving  user validation if column full

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ keep_going_eileen = True
 col = 10
 player_piece = "x"
 underl = "_"
+choice = []
+temp1 =True
 
 
 for i in range(6): # fill board variable
@@ -24,14 +26,17 @@ def draw_board(board):
 
 
 def check_column_full(col):
-   
+    global temp1
+    temp1 = True
     temp= board[0][col]
     print(temp)
     
     while board[0][col] != "_":
-        col = int(input("This column is full. Please choose again: "))
-
-    return col
+        temp1 = False
+        print("This column is full. Please choose again: ")
+           
+           
+    return col ,temp1
    
 
 
@@ -39,6 +44,8 @@ def take_input_from_player():
     global number_turns
     global col 
     global player_piece
+    global temp1
+    
     col = 10
     going = True
     if number_turns % 2 == 0:
@@ -48,28 +55,20 @@ def take_input_from_player():
         player = "Player 2"
         player_piece = "0"
 
-    while (col < 0) or (col > 6):
+    while (col < 0) or (col > 6) :
         try:
             col = int(input(f"{player} pick a column to drop an {player_piece} : "))
         except ValueError:
             print("Enter a NUMBER in range 0-6")
-
-
-
-
-    # while going:
-    #     if col in range(0, 7):
-    #         print("Valid column thanks very much you're very kind")
-    #         going = False
-    #     else:
-    #         print("NOT IN RANGE")
-    #         try:
-    #             col = int(input(f"{player} pick a column to drop an {player_piece} :"))
-    #         except ValueError:
-    #             print("Enetr a NUMBER in range 0-6")
+    
+    if board[0][col] != "_":
+       raise Exception("column full")
         
+        
+        
+    choice = col
             
-    choice = check_column_full(col)
+   
     for row in range(5, -1, -1):
         if board[row][choice] == "_":
                 board[row][choice] = player_piece
@@ -119,27 +118,20 @@ def check_win(piece):
                 print(f"{piece} has won!")
                 exit()
 
-    # check diagonals for line of 4
-    """
-    30 21 12 03                             i in range 3,5          30 21 12 03  0123 0123  
-    40 31 22 13 04                                                  31 22 13 04  0123 1234
-    50 41 32 23 14 05                                               32 23 14 05  0123 2345
-                                                                    33 24 15 06  0123 3456
-    31 22 13 04
-    41 32 23 14 05
-    51 42 33 24 15 06                       row 3 is the first one capable of having a line of 4  so start i at 3 and add 0ne each iter in inner loop
-                                            j  in range 0, 6
-    32 23 14 05
-    42 33 24 15 06
-    52 43 34 25 16
+    # check forwards  diagonals for line of 4
+   
+    for i in range(3,6):
+        for j in range(0,4):
+            if board[i][j] == piece and board[i-1][j+1] == piece and board[i-2][j+2] == piece and board[i-3][j+3] == piece:
+                print(f"{piece} has won!")
+                exit()
 
-    33 24 15 06
-    43 34 25 16
-    53 44 35 26
-    """
-    for i in range(4):
-        for j in range(3):
-            if board[j][i] == piece and board[j+1][i+1] == piece and board[j+2][i+2] == piece  and board[j+3][i+3] == piece:
+
+    # check backwards diagonals for line of 4 
+
+    for i in range(3,6):
+        for j in range(6,2,-1):
+            if board[i][j] == piece and board[i-1][j-1] == piece and board[i-2][j-2] == piece and board[i-3][j-3] == piece:
                 print(f"{piece} has won!")
                 exit()
 
