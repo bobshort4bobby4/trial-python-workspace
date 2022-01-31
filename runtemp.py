8d268bf3bd8c439d4110a2b2212caaa1a0e7db20
bobshort4bobby4 <treborken@gmail.com>
2022-01-31 18:07:27 +0000

    ADD oop version

--- runtemp.py
+++ runtemp.py
@@ -1,28 +1,99 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
 
-"""
-trial to see if i could parse a json file.
-"""
-import json
 
-horsedata = []
-for line in open('./data/5line-horsepro.json', 'r'):
-    horsedata.append(json.loads(line))
+class ColumnFullError(Error):
+    """Raised when the column is full"""
+    pass
 
-print(type(horsedata))
-print()
-print("lenght of the horsedate file is", len(horsedata))
-for i in range(len(horsedata)):
-    print(f"type of the {i} th line is", type(horsedata[i]))
 
-###for i in range(len(horsedata)):
-# #for key in horsedata[i].items():
-#        print(horsedata[i]['mc'])
-#print(horsedata[0])
-line = horsedata[0]
-print(horsedata[0]['mc'][0]['id'])
-print(len(horsedata[0]['mc']))
-print(len(horsedata[0]['mc'][0]))
-print(horsedata[0]['mc'][0]['marketDefinition'])
+
+
+class Board():
+    """
+    build the board the game is played on
+    """
+    
+
+    def __init__(self,):
+        self.board = self.build()
+        self.number_turns = 2
+       
+
+    def build(self):
+        board_width = 7
+        board_height = 6
+        self.board = []
+        for i in range(board_width - 1):
+            self.board.append([])
+
+        for i in range(board_height): # fill board variable
+            for j in range(board_width ):
+                self.board[i].insert(j , "_")
+        return self.board
+
+    def draw_board(self):        
+        print("")
+        print("    0 1 2 3 4 5 6 ")
+        print("__________________")
+        for i in range(6):
+                print(i, "~", end="|")
+                for j in range(7):
+                        print(self.board[i][j], end="|")
+                print("")
+
+
+    def whose_turn(self):
+        if self.number_turns % 2 == 0:
+            player_piece = "x"
+            player = "Player 1 "
+            
+            self.number_turns += 1
+        else:
+            player = "Player 2"
+            player_piece = "0"
+           
+            self.number_turns += 1
+        return player, player_piece
+
+    def take_move(self,player, player_piece):
+        col  = 10
+        while (col < 0) or (col > 6) :
+            try:
+                col = int(input(f"{player} pick a column to drop an {player_piece} : "))
+                if self.board[0][col] != "_":
+                    raise ColumnFullError
+            except ValueError:
+                    print("Enter a NUMBER in range 0-6")
+            except ColumnFullError:
+                    print("column full")
+
+        return col
+
+
+
+#input = Input()
+
+
+
+#board = Board(1)
+
+
+#board.draw_board()
+
+
+def play_game():
+
+    game = Board()
+
+    player, player_piece = game.whose_turn()
+    choice = game.take_move(player, player_piece)
+   
+    print("the choice is", choice)
+
+
+
+
+if __name__ == '__main__':
+    play_game()
